9b0cbd2ba4e429d5960ac1c7837a963dae52c5a5
xiaoheng666 <1919196455@qq.com>
2020-04-11 13:51:15 +0800

    commit second

--- test.php
+++ test.php
@@ -1,2 +1,6 @@
 /storage/emulated/0/AppProjects/github/
 https://github.com/xiaoheng666/openfireandsmack.git
+://github.com/xiaoheng666/openfireandsmack.git
+
+
+/storage/emulated/0/AppProjects/githubtest/openfireandsmack/eb https://mirrors.tuna.tsinghua.edu.cn/termux stable main
