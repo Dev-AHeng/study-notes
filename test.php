bcd6abaa4151fa8042042cf34a321eaa4a814079
xiaoheng666 <1919196455@qq.com>
2020-04-11 14:19:56 +0800

    删除了一些文件

--- test.php
+++
@@ -1,6 +0,0 @@
-/storage/emulated/0/AppProjects/github/
-https://github.com/xiaoheng666/openfireandsmack.git
-://github.com/xiaoheng666/openfireandsmack.git
-
-
-/storage/emulated/0/AppProjects/githubtest/openfireandsmack/eb https://mirrors.tuna.tsinghua.edu.cn/termux stable main
